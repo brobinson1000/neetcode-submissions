89001eb50ca62c7e95a10bfc49e48acbdb70506c
Brandon Robinson <157557286+brobinson1000@users.noreply.github.com>
2026-07-12 13:58:46 -0500

    Add: missing-number - submission-3

---
+++ Data Structures & Algorithms/missing-number/submission-3.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+
+        int sum = std::accumulate(nums.begin(), nums.end(), 0);
+        
+        return (n * (n + 1) / 2) - sum;
+
+    }
+};
